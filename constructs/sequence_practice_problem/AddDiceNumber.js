c1899e34b75caa5d361c7f4c379abec5b3678bad
Darshan Yadav <121230952+drshny03@users.noreply.github.com>
2025-03-04 11:19:46 +0530

    Delete constructs directory

--- constructs/sequence_practice_problem/AddDiceNumber.js
+++
@@ -1,6 +0,0 @@
-let firstDice = Math.floor(Math.random() * 6 +1);
-let secondDice = Math.floor(Math.random() * 6 +1);
-
-//calculate result
-let result = firstDice + secondDice;
-console.log(result);
