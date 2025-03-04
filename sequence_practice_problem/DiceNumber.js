16014365315177939d55f3d9c1ac9a733caa97de
drshny03 <darshanyadav7049@gmail.com>
2025-03-04 11:29:54 +0530

    [Darshan] Add. Repetition Practice Problems with for loop

--- sequence_practice_problem/DiceNumber.js
+++
@@ -1,3 +0,0 @@
-let diceNumber = Math.floor(Math.random() * 6 +1)
-//print result
-console.log(diceNumber)
--- sequence_practice_problem/SingleDigit.js
+++
@@ -1,5 +0,0 @@
-//get a random single digit
-let number = Math.floor(Math.random()*10);
-
-//print result
-console.log(number);
