c39d2428cf27347edc8309751ec497f5ef134cac
drshny03 <darshanyadav7049@gmail.com>
2025-03-04 11:28:53 +0530

    [Darshan] Add. sequence practice problem solutions

---
+++ sequence_practice_problem/AddDiceNumber.js
@@ -0,0 +1,6 @@
+let firstDice = Math.floor(Math.random() * 6 +1);
+let secondDice = Math.floor(Math.random() * 6 +1);
+
+//calculate result
+let result = firstDice + secondDice;
+console.log(result);
---
+++ sequence_practice_problem/SingleDigit.js
@@ -0,0 +1,5 @@
+//get a random single digit
+let number = Math.floor(Math.random()*10);
+
+//print result
+console.log(number);
